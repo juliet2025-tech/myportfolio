e2189a065075a38f95cba9daf430298ee2fe5b94
ojide chibundo <chibundojuliet21@gmail.com>
2026-03-26 16:07:00 +0100

    final commit

--- my-portfolio/src/components/project.js
+++ my-portfolio/src/components/project.js
@@ -11,11 +11,11 @@ function Projects() {
 
 The application features categorized entries for income and expenses, making it easy to organize financial data. Users can add, edit, and delete transactions seamlessly, ensuring full control over their records.</p>
 
-        <a href="https://github.com/juliet2025-tech/budget-tracker" target="_blank">
+        <a href="https://github.com/juliet2025-tech/budget-tracker" target="_blank" rel="noreferrer">
           GitHub
         </a>
 
-        <a href="https://budget-tracker-by-jules.vercel.app" target="_blank">
+        <a href="https://budget-tracker-by-jules.vercel.app" target="_blank" rel="noreferrer">
          Live Demo
         </a>
 
@@ -26,11 +26,11 @@ The application features categorized entries for income and expenses, making it 
         <h3>Weather Forecast App</h3>
         <p>A weather forecasting app built with React that displays real-time weather data.</p>
 
-        <a href="https://github.com/juliet2025-tech/weather-App" target="_blank">
+        <a href="https://github.com/juliet2025-tech/weather-App" target="_blank" rel="noreferrer">
           GitHub
         </a>
 
-        <a href="https://weather-app-wsco.vercel.app/" target="_blank">
+        <a href="https://weather-app-wsco.vercel.app/" target="_blank" rel="noreferrer">
           Live Demo
         </a>
       </div>
@@ -39,11 +39,11 @@ The application features categorized entries for income and expenses, making it 
         <h3>To-Do List App</h3>
         <p>A task management app that allows users to add, delete and track tasks.</p>
 
-        <a href="https://github.com/juliet2025-tech/My-todo-list" target="_blank">
+        <a href="https://github.com/juliet2025-tech/My-todo-list" target="_blank" rel="noreferrer">
           GitHub
         </a>
 
-        <a href="https://todo-it-by-jules.netlify.app/" target="_blank">
+        <a href="https://todo-it-by-jules.netlify.app/" target="_blank" rel="noreferrer">
          Live Demo
         </a>
         </div>
